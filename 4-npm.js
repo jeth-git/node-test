20f18989ee818bbbadd0eb147e712c3ff3fc4b5f
jet-git <jethrotabue@gmail.com>
2026-03-24 09:10:04 +0100

    second commit

--- 4-npm.js
+++ 4-npm.js
@@ -28,7 +28,10 @@
 // npm install -g <packageName>
 //sudo npm install -g <packageName> (mac)
 
-// a devdependency is a category of local dependency that are used while you are actively writing code, testing, or building your app. e.g typescript, ESLint(linter), Jest(testing), or Webpack(bundler)
+// a devdependency is a category of local dependency that are used while you are actively writing code, testing, or building your app. e.g typescript, ESLint(linter), Jest(testing), or Webpack(bundler). 
+
+// To install devdependecy, npm install <package-name> -D or npm install <package-name> --save-dev
+
 // npx (NODE PACKAGE EXECUTE)
 
 // package.json - manifest file (stores important info about project/package)
@@ -38,7 +41,9 @@
 
 // node_modules (the artefact) is the "heavy lifting" that contains the actual source code of the dependency while package.json(the manifest) is "brain" the lightweight json file use to store the dependencies metadata(name, version, author) it tells the other devs(your server) exactly what needs to be installed for the app to work
 
-// the package-lock.json file is a file that contains the exact details(like dependencies and their sub-dependencies if they exist, the exact version of the dependency to which the original project was developped) about the dependecy. It is very crucial during shared works. 
+// the package-lock.json file is a file that contains the exact details(like dependencies and their sub-dependencies if they exist, the exact version of the dependency to which the original project was developped) about the dependecy. It is very crucial during shared works.
+
+// NVM(Node Version Module)
 
 const _ = require('lodash')
 
@@ -46,3 +51,16 @@ const items = [1, [2, [3, [4]]]]
 const newItems = _.flattenDeep(items)
 
 console.log(newItems)
+
+// nodemon is a dependency used to "watch" changes in the app while coding. It performs "Cold Restart" , and it is used for the backend.
+// NB: The dependency used to "watch" changes in the app while coding for the frontend environment(React/Vite) is known as React Dev Server
+
+// Semantic Versioning
+/*
+    4.17.20
+    1.2.3
+    major.minor.patch
+    breaking changes.backward compatible.bug fix
+*/
+
+// Reference: The basics of package.json(nodesource.com)
